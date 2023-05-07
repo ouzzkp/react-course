f457626dff9993d84378ae53ba2021dede2f1d64
ouzzkp <oguzkaan3096@gmail.com>
2023-05-07 12:31:57 +0300

    Setup the Reacstrap and Bootstrap

--- intro/src/App.js
+++ intro/src/App.js
@@ -2,14 +2,27 @@ import React from "react";
 import Navi from "./Navi";
 import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
+import { Container, Row, Col } from 'reactstrap'
 
 
 function App() {
   return (
     <div>
-      <Navi />
-      <CategoryList />
-      <ProductList />
+      <Container>
+        <Row>
+          <Row>
+            <Navi />
+          </Row>
+          <Col xs="3">
+          <CategoryList />
+          </Col>
+          <Col xs="9">
+          <ProductList />
+        </Col>
+          </Row>
+      </Container>
+
+
     </div>
   );
 }
--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export default class CategoryList extends Component {
     render() {
         return (
             <div>
                 <h3>Category List</h3>
+                <ListGroup>
+                    <ListGroupItem>Ev/Bahçe</ListGroupItem>
+                    <ListGroupItem>Elektronik</ListGroupItem>
+                    <ListGroupItem>Kırtasiye</ListGroupItem>
+                    <ListGroupItem>Sağlık</ListGroupItem>
+                    <ListGroupItem>Spor</ListGroupItem>
+                </ListGroup>
             </div>
         )
     }
