6a167a71414cd22670d30f490995c224d08512c8
ouzzkp <oguzkaan3096@gmail.com>
2023-05-07 12:09:53 +0300

    Introduction to React
    Create a first component

---
+++ intro/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+
+
+function App() {
+  return (
+    <div>
+      <Navi />
+      <CategoryList />
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
